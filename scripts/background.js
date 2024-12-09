9ae3a0ff17ff24ef6f0d3c3f022e7f2b6839d00b
Joel McCandless <mail@joelmccandless.com>
2024-12-08 22:21:09 -0500

    get tasks api working

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,34 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        chrome.identity.getAuthToken({interactive: true}, function(token) {
-            console.log(token);
-        });      
-        sendResponse({response: token});
+        (async () => {
+            const token = await getAuthToken(true);
+            const response = await fetch("https://www.googleapis.com/tasks/v1/users/@me/lists", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                },
+            });
+
+            console.log("response", response);
+
+            const json = await response.json();
+            console.log("response", json);
+            sendResponse(json);
+        })();
+        return true;
     }
 );
+
+function getAuthToken(interactive) {
+    return new Promise((resolve, reject) => {
+      chrome.identity.getAuthToken({ interactive }, (token) => {
+        if (chrome.runtime.lastError || !token) {
+          reject(chrome.runtime.lastError.message);
+        } else {
+          resolve(token);
+        }
+      });
+    });
+  }
+  
