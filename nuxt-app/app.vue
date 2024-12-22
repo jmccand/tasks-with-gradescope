497d687de8198e4de4a5638793fcbc766325728d
Joel McCandless <mail@joelmccandless.com>
2024-12-21 18:20:15 -0600

    create tasklist component, add example lists

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,6 +1,18 @@
+<script setup>
+import TaskList from '~/components/TaskList.vue'
+
+const taskLists = ref([
+  [
+    { id: 1, name: 'Task 1' },
+    { id: 2, name: 'Task 2' }
+  ],
+  [
+    { id: 3, name: 'Task 3' },
+    { id: 4, name: 'Task 4' }
+  ]
+])
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <TaskList v-for="tasks in taskLists" :tasks="tasks" :key="tasks[0].id" />
 </template>
---
+++ nuxt-app/components/TaskList.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  tasks: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+<style scoped>
+div {
+  border: 2px solid #000;
+  border-radius: 4px;
+  padding: none;
+  margin: 4px 0;
+  width: 300px;
+}
+ul {
+    margin: none;
+}
+</style>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="task in tasks" :key="task.id">
+            {{ task.name }}
+            </li>
+        </ul>
+    </div>
+</template>
